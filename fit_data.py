b5a273e909d85b9345bdf545d87a8c859f890222
ralex0 <ralex0@users.noreply.github.com>
2019-02-07 13:34:11 -0500

    Add loads for a bunch of data and functions for making figs

--- fit_data.py
+++ fit_data.py
@@ -1,3 +1,6 @@
+import time
+import warnings
+
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -142,6 +145,169 @@ def load_few_PS_data_Jan4():
              for path in paths]
     return holos, zpos
 
+
+def load_few_PS_data_Jan8():
+    camera_resolution = 5.9633 # px / um
+    metadata = {'spacing' : 1 / camera_resolution,
+                    'medium_index' : 1.348,
+                    'illum_wavelen' : .660,
+                    'illum_polarization' : (1, 0)}
+    position = [617, 291]
+
+    holonums = range(21)
+    zpos = np.linspace(-20, 20, 21)
+    paths = ["data/Polystyrene-60xWater-010819/raw_greyscale/im_" +  f"{num}".zfill(3) + ".png"
+             for num in holonums] 
+    holos = [mlf.load_bgdivide_crop(path=path, metadata=metadata, 
+                                particle_position=position, bg_prefix="bkg", df_prefix="dark") 
+             for path in paths]
+    return holos, zpos
+
+def load_few_PS_data_Jan10():
+    camera_resolution = 5.9633 # px / um
+    metadata = {'spacing' : 1 / camera_resolution,
+                    'medium_index' : 1.348,
+                    'illum_wavelen' : .660,
+                    'illum_polarization' : (1, 0)}
+    position = [640, 306]
+
+    holonums = range(51)
+    zpos = np.linspace(25, -25, 51) - 2.5
+    paths = ["data/Mixed-60xWater-011019/greyscale-PS/im_" +  f"{num}".zfill(3) + ".png"
+             for num in holonums] 
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        holos = [mlf.load_bgdivide_crop(path=path, metadata=metadata, 
+                                        particle_position=position, 
+                                        bg_prefix="bkg", 
+                                        df_prefix="dark") 
+                 for path in paths]
+    return holos, zpos
+
+def load_few_BigPS_data_Jan10():
+    camera_resolution = 5.9633 # px / um
+    metadata = {'spacing' : 1 / camera_resolution,
+                    'medium_index' : 1.348,
+                    'illum_wavelen' : .660,
+                    'illum_polarization' : (1, 0)}
+    position = [901, 271]
+
+    holonums = range(51)
+    zpos = np.linspace(-25, 25, 51)
+    paths = ["data/Mixed-60xWater-011019/greyscale-PS-big/im_" +  f"{num}".zfill(3) + ".png"
+             for num in holonums] 
+    holos = [mlf.load_bgdivide_crop(path=path, metadata=metadata, 
+                                particle_position=position, bg_prefix="bkg", df_prefix="dark") 
+             for path in paths]
+    return holos, zpos
+
+def load_few_Si_data_Jan10():
+    camera_resolution = 5.9633 # px / um
+    metadata = {'spacing' : 1 / camera_resolution,
+                    'medium_index' : 1.348,
+                    'illum_wavelen' : .660,
+                    'illum_polarization' : (1, 0)}
+    position = [607, 339]
+
+    holonums = range(51)
+    zpos = np.linspace(25, -25, 51) - 0.5
+    paths = ["data/Mixed-60xWater-011019/greyscale-silica/im_" +  f"{num}".zfill(3) + ".png"
+             for num in holonums] 
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        holos = [mlf.load_bgdivide_crop(path=path, metadata=metadata, 
+                                        particle_position=position, 
+                                        bg_prefix="bkg", df_prefix="dark") 
+             for path in paths]
+    return holos, zpos
+
+
+def load_PS_data_Feb6():
+    camera_resolution = 5.6983 # px / um
+    metadata = {'spacing' : 1 / camera_resolution,
+                    'medium_index' : 1.348,
+                    'illum_wavelen' : .660,
+                    'illum_polarization' : (1, 0)}
+    position = [266, 680]
+
+    holonums = np.arange(14, 137)
+    zpos = np.linspace(13.5, -17, len(holonums))
+    paths = ["data/Polystyrene-60xWater-020619/im" +  f"{num}".zfill(4) + ".tif"
+             for num in holonums]
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        refimg = hp.load_image(paths[0], **metadata)
+        bkg = mlf.load_bkg(paths[0], bg_prefix='bg', refimg=refimg)
+        dark = mlf.load_dark(paths[0], df_prefix='dark', refimg=refimg)
+        holos = [mlf.load_bgdivide_crop_v2(path=path, metadata=metadata, 
+                                        particle_position=position, 
+                                        bkg=bkg, dark=dark) 
+                 for path in paths]
+    return holos, zpos
+
+def load_PS_data_Feb6_alt():
+    camera_resolution = 5.6983 # px / um
+    metadata = {'spacing' : 1 / camera_resolution,
+                    'medium_index' : 1.348,
+                    'illum_wavelen' : .660,
+                    'illum_polarization' : (1, 0)}
+    position = [371, 860]
+
+    holonums = range(134)
+    zpos = np.linspace(17, -17, len(holonums))
+    paths = ["data/Polystyrene-60xWater-020619/raw0/im" +  f"{num}".zfill(4) + ".tif"
+             for num in holonums]
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        refimg = hp.load_image(paths[0], **metadata)
+        bkg = mlf.load_bkg(paths[0], bg_prefix='bg', refimg=refimg)
+        dark = mlf.load_dark(paths[0], df_prefix='dark', refimg=refimg)
+        holos = [mlf.load_bgdivide_crop_v2(path=path, metadata=metadata, 
+                                        particle_position=position, 
+                                        bkg=bkg, dark=dark) 
+                 for path in paths]
+    return holos, zpos
+
+# def load_few_PS_sedemintation_data_Jan24():
+#     camera_resolution = 5.6983 # px / um
+#     metadata = {'spacing' : 1 / camera_resolution,
+#                     'medium_index' : 1.348,
+#                     'illum_wavelen' : .660,
+#                     'illum_polarization' : (1, 0)}
+#     position = [242, 266]
+
+#     holonums = [19, 20]
+#     zpos = np.linspace(25, -25, 51) - 0.5
+#     paths = ["data/Polystyrene2-4um-60xWater-012419/raw/image" +  f"{num}".zfill(4) + ".png"
+#              for num in holonums] 
+#     with warnings.catch_warnings():
+#         warnings.simplefilter("ignore")
+#         holos = [mlf.load_bgdivide_crop(path=path, metadata=metadata, 
+#                                         particle_position=position, 
+#                                         bg_prefix="bkg", df_prefix="dark") 
+#              for path in paths]
+#     return holos, zpos
+
+def load_PS_data_reza():
+    resolution = 1/.194 # pixels per um
+    metadata = {'spacing' : 1 / resolution,
+                'medium_index' : 1.34,
+                'illum_wavelen' : .660,
+                'illum_polarization' : (1, 0)}
+    PS_location = [258, 255]
+
+    holonums = range(80)
+    zpos = np.linspace(20, -20, 80)
+    paths = ["data/Polystyrene-Reza/image" +  f"{num}".zfill(4) + ".tif"
+             for num in holonums] 
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        holos = [mlf.load_bgdivide_crop(path=path, metadata=metadata, 
+                                        particle_position=position, 
+                                        bg_prefix="bg") 
+             for path in paths]
+    return holos, zpos
+
 def make_guess_parameters(zpos, n, r):
     return [{'z': z, 'n': n, 'r': r} for z in zpos]
 
@@ -162,6 +328,22 @@ def compare_imgs(im1, im2, titles=['im1', 'im2']):
     plt.imshow(im2, interpolation="nearest", vmin=vmin, vmax=vmax)
     plt.colorbar()
     plt.title(titles[1])
+    plt.show()
+
+
+def compare_holos(*holos, titles=None, cmap="Greys"):
+    ims = [holo.values.squeeze() for holo in holos]
+    vmax = np.max(ims)
+    vmin = np.min(ims)
+
+    plt.figure(figsize=(5*len(ims),5))
+    plt.gray()
+    for index, im in enumerate(ims):
+        plt.subplot(1, len(ims), index + 1)
+        plt.imshow(im, interpolation="nearest", vmin=vmin, vmax=vmax, cmap=cmap)
+        plt.colorbar()
+        if titles:
+            plt.title(titles[index])
     plt.show()
 
 def fit_data():
@@ -205,27 +387,52 @@ def compare_fit_Jan4_data():
     moerrsq = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters, theory='mieonly') for data, fit in zip(PS_data, mofit)]
     mlerrsq = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters) for data, fit in zip(PS_data, mlfit)]
 
+def make_stack_figures(data, fits, n=None, r=None, z_positions=None):
+    scatterers = [fit.scatterer for fit in fits]
+    z = [fit.scatterer.center[2] for fit in fits] if z_positions is None else z_positions
+    for scatterer, z_pos in zip(scatterers, z):
+        scatterer.n = n if n is not None else scatterer.n
+        scatterer.r = r if r is not None else scatterer.r
+        scatterer.center[2] = z_pos if z_positions is not None else scatterer.center[2]
+
+    model_holos = [calc_holo(dt, scatterer, theory=MieLens(lens_angle=1.0)) 
+                   for dt, scatterer in zip(data, scatterers)]
+
+    data_stack_xz = np.vstack([dt.values.squeeze()[50,:] for dt in data])
+    data_stack_yz = np.vstack([dt.values.squeeze()[:,50] for dt in data])
+
+    model_stack_xz = np.vstack([holo.values.squeeze()[50,:] for holo in model_holos])
+    model_stack_yz = np.vstack([holo.values.squeeze()[:,50] for holo in model_holos])
+    
+    return data_stack_xz, data_stack_yz, model_stack_xz, model_stack_yz
+
+
 if __name__ == '__main__':
-    PS_data, PS_zpos = load_few_PS_data_Jan4()
+    # Load PS data
+    PS_data, PS_zpos = load_few_PS_data_Jan10()
     PS_guess = make_guess_parameters(PS_zpos, n=1.58, r=0.5)
 
-    data = PS_data[2]
-    guess = PS_guess[2]
-    guess['z'] = -10
-    fit = mlf.fit_mielens(data, guess)
-    gop = mlf.globalop_mielens(data, guess)
-    polished_guess = {'z': gop.parameters['center.2'], 'n': gop.parameters['n'], 'r': gop.parameters['r']}
-    polished_gop = mlf.fit_mielens(data, polished_guess)
-
-    fit_err = mlf.calc_err_sq(data, fit.scatterer, **fit.parameters, theory='mielens')
-    gop_err = mlf.calc_err_sq(data, gop.scatterer, **gop.parameters, theory='mielens')
-    pgop_err = mlf.calc_err_sq(data, polished_gop.scatterer, **polished_gop.parameters, theory='mielens')
-
-    # mofit = [mlf.fit_mieonly(data, guess) for data, guess in zip(PS_data, PS_guess)]
-    # mlfit = [mlf.fit_mielens(data, guess) for data, guess in zip(PS_data, PS_guess)]
-
-    # moholo = [calc_holo(data, fit.scatterer, scaling=fit.parameters['alpha']) for data, fit in zip(PS_data, mofit)]   
-    # mlholo = [calc_holo(data, fit.scatterer, theory=MieLens(lens_angle=fit.parameters['lens_angle'])) for data, fit in zip(PS_data, mlfit)]
+    # Load fits I've already done
+    mofit_PS = [hp.load(f"fits/PSJan10/mofit{num}.h5") for num in [f"{num}".zfill(3) for num in range(len(PS_data))]]
+    mlfit_PS = [hp.load(f"fits/PSJan10/mlfit{num}.h5") for num in [f"{num}".zfill(3) for num in range(len(PS_data))]]
+    moholo_PS = [calc_holo(data, fit.scatterer, scaling=fit.parameters['alpha']) for data, fit in zip(PS_data, mofit_PS)]   
+    mlholo_PS = [calc_holo(data, fit.scatterer, theory=MieLens(lens_angle=fit.parameters['lens_angle'])) for data, fit in zip(PS_data, mlfit_PS)]
     
-    # moerrsq = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters, theory='mieonly') for data, fit in zip(PS_data, mofit)]
-    # mlerrsq = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters) for data, fit in zip(PS_data, mlfit)]
+    moerrsq_PS = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters, theory='mieonly') for data, fit in zip(PS_data, mofit_PS)]
+    mlerrsq_PS = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters) for data, fit in zip(PS_data, mlfit_PS)]
+
+
+    # Make figure 3 with this function
+    data_stack_xz, data_stack_yz, model_stack_xz, model_stack_yz = make_stack_figures(PS_data, mlfit_PS, n=1.41, r=0.5, z_positions=np.linspace(26.5, -23.5, 51))
+
+    compare_imgs(data_stack_xz, model_stack_xz)
+    compare_imgs(data_stack_yz, model_stack_yz)
+
+    # This is for Fitting the Silica data
+    #mofit_Si = [mlf.fit_mieonly(data, guess) for data, guess in zip(Si_data, Si_guess)]
+    #mlfit_Si = [mlf.fit_mielens(data, guess) for data, guess in zip(Si_data, Si_guess)]
+    #moholo_Si = [calc_holo(data, fit.scatterer, scaling=fit.parameters['alpha']) for data, fit in zip(Si_data, mofit_Si)]   
+    #mlholo_Si = [calc_holo(data, fit.scatterer, theory=MieLens(lens_angle=fit.parameters['lens_angle'])) for data, fit in zip(Si_data, mlfit_Si)]
+    
+    #moerrsq_Si = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters, theory='mieonly') for data, fit in zip(Si_data, mofit_Si)]
+    #mlerrsq_Si = [mlf.calc_err_sq(data, fit.scatterer, **fit.parameters) for data, fit in zip(Si_data, mlfit_Si)]
