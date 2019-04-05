91b689eeea5ae59163ed043dedd0faf5812a30e7
Brian Leahy <bdl48@cornell.edu>
2019-04-05 10:23:14 -0400

    fit_sed_data: changes for pep8 and for Brian to run code

--- fit_sedimentation_data.py
+++ fit_sedimentation_data.py
@@ -17,21 +17,25 @@ import mielensfit as mlf
 from lmfitFitter import Fitter
 import inout
 
+
+__TIMER_CLICK__ = time.time()
+
+
 def test_fit(dt, guess):
     fitter_ml = Fitter(theory='mielens')
     fitter_mo = Fitter(theory='mieonly')
 
     print("Fitting with mielens...")
     t0 = time.time()
-    ml_fit = fitter_ml.fit(dt, guess)    
+    ml_fit = fitter_ml.fit(dt, guess)
     ml_time = timedelta(seconds=(time.time() - t0))
-    print(f"Done! Took {ml_time} s")
+    print("Done! Took {} s".format(ml_time))
     print(report_fit(ml_fit))
 
     print("Fitting with mieonly...")
     mo_fit = fitter_mo.fit(dt, guess)
     mo_time = timedelta(seconds=(time.time() - t0 - ml_time.total_seconds()))
-    print(f"Done! Took {mo_time} s")
+    print("Done! Took {} s".format(mo_time))
     print(report_fit(mo_fit))
 
     mo_holo = fitter_mo._calc_model(mo_fit.params, dt)
@@ -51,7 +55,8 @@ def compare_holos(*holos, titles=None, cmap="gray"):
     plt.gray()
     for index, im in enumerate(ims):
         plt.subplot(1, len(ims), index + 1)
-        plt.imshow(im, interpolation="nearest", vmin=vmin, vmax=vmax, cmap=cmap)
+        plt.imshow(
+            im, interpolation="nearest", vmin=vmin, vmax=vmax, cmap=cmap)
         plt.colorbar()
         if titles:
             plt.title(titles[index])
@@ -62,7 +67,6 @@ def _guesses_from_old(fits):
     return list(fits.values())
 
 
-__TIMER_CLICK__ = time.time()
 def tick_tock():
     global __TIMER_CLICK__
     last_time =  __TIMER_CLICK__
@@ -89,7 +93,7 @@ def cascade_fit(data, first_guess, fitter, particle, bottom_frame):
         if fitter.theory == 'mielens':
             keys = ['z', 'n', 'r', 'lens_angle']
         guess = {k: next_fit.params[k] for k in keys}
-    return fits 
+    return fits
 
 Particle = namedtuple("Particle", ["radius", "density"])
 # We take the radii as the median radii from mielens. We use the
@@ -100,7 +104,8 @@ SILICA_PARTICLE = Particle(radius=0.7826, density=2.0)
 POLYSTYRENE_PARTICLE = Particle(radius=1.168, density=1.05)
 VISCOSITY_WATER = 8.9e-4  # in mks units = Pa*s
 
-def _calc_particle_trajectory(times, particle, initial_z_position, bottom_frame=None):
+def _calc_particle_trajectory(
+        times, particle, initial_z_position, bottom_frame=None):
     # 1. Calculate the velocity, using meter-kilogram-second units:
     radius = particle.radius * 1e-6
     density = (particle.density - 1) * 1e3  # 1 g / cc = 1e3 kg / m^3
@@ -121,17 +126,18 @@ def _calc_particle_trajectory(times, particle, initial_z_position, bottom_frame=
 
     return trajectory
 
+
 def fit_all_data():
     tick_tock()
     si_data = inout.load_silica_sedimentation_data()[0]
     ps_data = inout.load_polystyrene_sedimentation_data()[0]
     si_times = np.load("./fits/sedimentation/Si_frame_times.npy")
     ps_times = np.load("./fits/sedimentation/PS_frame_times.npy")
-    print(f"Time to load data: {tick_tock()}")
+    print("Time to load data: {}".format(tick_tock()))
 
     fitter_ml = Fitter(theory='mielens')
     fitter_mo = Fitter(theory='mieonly')
-    
+
     old_ps_fits_mo, old_ps_fits_ml = inout.load_polystyrene_sedimentation_fits()
     old_si_fits_mo, old_si_fits_ml = inout.load_silica_sedimentation_fits()
 
@@ -146,37 +152,49 @@ def fit_all_data():
     si_zpos = _calc_particle_trajectory(si_times, SILICA_PARTICLE,
                                         si_initial_z, si_bottom_frame)
 
-    ps_guesses = [{'n': 1.58, 'r': POLYSTYRENE_PARTICLE.radius, 'z': z} for z in ps_zpos]
-    si_guesses = [{'n': 1.43, 'r': SILICA_PARTICLE.radius, 'z': z} for z in si_zpos]
+    ps_guesses = [{'n': 1.58, 'r': POLYSTYRENE_PARTICLE.radius, 'z': z}
+                  for z in ps_zpos]
+    si_guesses = [{'n': 1.43, 'r': SILICA_PARTICLE.radius, 'z': z}
+                  for z in si_zpos]
 
     try:
         pool = Pool(4)
         tick_tock()
-        ps_fits_mo = pool.starmap(fitter_mo.fit, zip(ps_data[:20], ps_guesses[:20]))
-        ps_fits_ml = pool.starmap(fitter_ml.fit, zip(ps_data, ps_guesses))
-        si_fits_mo = pool.starmap(fitter_mo.fit, zip(si_data[:43], si_guesses[:43]))
-        si_fits_ml = pool.starmap(fitter_ml.fit, zip(si_data, si_guesses))
-        print(f"Time to do fits: {tick_tock()}")
+        ps_fits_mo = pool.starmap(
+            fitter_mo.fit, zip(ps_data[:20], ps_guesses[:20]))
+        ps_fits_ml = pool.starmap(
+            fitter_ml.fit, zip(ps_data, ps_guesses))
+        si_fits_mo = pool.starmap(
+            fitter_mo.fit, zip(si_data[:43], si_guesses[:43]))
+        si_fits_ml = pool.starmap(
+            fitter_ml.fit, zip(si_data, si_guesses))
+        print("Time to do fits: {}".format(tick_tock()))
     finally:
         pool.close()
         pool.join()
 
     return ps_fits_mo, ps_fits_ml, si_fits_mo, si_fits_ml
 
+
 def save_results(ps_fits_mo, ps_fits_ml, si_fits_mo, si_fits_ml):
     si_params_ml = OrderedDict()
+    savefolder = "fits/sedimentation/best_of_03-27_and_04-02/"
     for i, fit in enumerate(si_fits_ml):
         si_params_ml.update({str(i): fit.params.valuesdict()})
-        inout.save_pickle(fit,
-            f"fits/sedimentation/best_of_03-27_and_04-02/Si_mielens/{inout.zfill(i)}.pkl")
-    with open('fits/sedimentation/best_of_03-27_and_04-02/mielens_sedimentation_params_Si.json', 'w') as f:
+        pickle_filename = os.path.join(
+            savefolder, "Si_mielens/{}.pkl".format(inout.zfill(i)))
+        inout.save_pickle(fit, pickle_filename)
+
+    json_filename = os.path.join(
+        savefolder, 'mielens_sedimentation_params_Si.json')
+    with open(json_filename, 'w') as f:
         json.dump(si_params_ml, f, indent=4)
 
     si_params_mo = OrderedDict()
     for i, fit in enumerate(si_fits_mo):
         si_params_mo.update({str(i): fit.params.valuesdict()})
         inout.save_pickle(fit,
-            f"fits/sedimentation/best_of_03-27_and_04-02/Si_mieonly/{inout.zfill(i)}.pkl")
+            "fits/sedimentation/best_of_03-27_and_04-02/Si_mieonly/{}.pkl".format(inout.zfill(i)))
     with open('fits/sedimentation/best_of_03-27_and_04-02/mieonly_sedimentation_params_Si.json', 'w') as f:
         json.dump(si_params_mo, f, indent=4)
 
@@ -184,7 +202,7 @@ def save_results(ps_fits_mo, ps_fits_ml, si_fits_mo, si_fits_ml):
     for i, fit in enumerate(si_fits_mo):
         ps_params_ml.update({str(i): fit.params.valuesdict()})
         inout.save_pickle(fit,
-            f"fits/sedimentation/best_of_03-27_and_04-02/PS_mielens/{inout.zfill(i)}.pkl")
+            "fits/sedimentation/best_of_03-27_and_04-02/PS_mielens/{}.pkl".format(inout.zfill(i)))
     with open('fits/sedimentation/best_of_03-27_and_04-02/mielens_sedimentation_params_PS.json', 'w') as f:
         json.dump(ps_params_ml, f, indent=4)
 
@@ -192,9 +210,10 @@ def save_results(ps_fits_mo, ps_fits_ml, si_fits_mo, si_fits_ml):
     for i, fit in enumerate(ps_fits_mo):
         ps_params_mo.update({str(i): fit.params.valuesdict()})
         inout.save_pickle(fit,
-            f"fits/sedimentation/best_of_03-27_and_04-02/PS_mieonly/{inout.zfill(i)}.pkl")
+            "fits/sedimentation/best_of_03-27_and_04-02/PS_mieonly/{}.pkl".format(inout.zfill(i)))
     with open('fits/sedimentation/best_of_03-27_and_04-02/mieonly_sedimentation_params_PS.json', 'w') as f:
         json.dump(ps_params_mo, f, indent=4)
+
 
 if __name__ == '__main__':
     tick_tock()
@@ -202,18 +221,18 @@ if __name__ == '__main__':
     ps_data = inout.load_polystyrene_sedimentation_data()[0]
     si_times = np.load("./fits/sedimentation/Si_frame_times.npy")
     ps_times = np.load("./fits/sedimentation/PS_frame_times.npy")
-    print(f"Time to load data: {tick_tock()}")
+    print("Time to load data: {}".format(tick_tock()))
 
     fitter_ml = Fitter(theory='mielens')
     fitter_mo = Fitter(theory='mieonly')
-    
+
     tick_tock()
     restarted_ps_fits_mo, restarted_ps_fits_ml = inout.load_polystyrene_sedimentation_fits("03-27")
     restarted_si_fits_mo, restarted_si_fits_ml = inout.load_silica_sedimentation_fits("03-27")
 
     fresh_ps_fits_mo, fresh_ps_fits_ml = inout.load_polystyrene_sedimentation_fits("04-02")
     fresh_si_fits_mo, fresh_si_fits_ml = inout.load_silica_sedimentation_fits("04-02")
-    print(f"Time to load fits: {tick_tock()}")
+    print("Time to load fits: {}".format(tick_tock()))
 
     # ps_fits_mo, ps_fits_ml, si_fits_mo, si_fits_ml = fit_all_data()
     # save_results(ps_fits_mo, ps_fits_ml, si_fits_mo, si_fits_ml)
