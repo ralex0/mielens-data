5cc27770e2eb71a7afada6606912bedc333ab902
Brian Leahy <bdl48@cornell.edu>
2019-04-05 15:09:10 -0400

    modifying mcmcdata to work with py3.5

--- mcmc_data.py
+++ mcmc_data.py
@@ -13,6 +13,8 @@ def tick_tock():
     return timedelta(seconds=(current_time - last_time))
 
 
+# TODO:
+# Time mielens with npixels=1e2 vs npixels=1e3 vs npixels=1e4
 if __name__ == '__main__':
     data, zpos = inout.load_polystyrene_sedimentation_data()
     fits_mo, fits_ml = inout.load_polystyrene_sedimentation_params("03-27")
@@ -23,24 +25,24 @@ if __name__ == '__main__':
     guess_mo = fits_mo['1']
     guess_ml = fits_ml['1']
 
-    mcmc_kws = {'burn': 0, 'steps': 1000, 'nwalkers': 100, 
+    mcmc_kws = {'burn': 0, 'steps': 2, 'nwalkers': 100,
                 'thin': 1, 'workers': 4, 'ntemps': 5}
 
     print("Starting mieonly mcmc")
     tick_tock()
-    mcmc_mo, fit_mo = mieonlyFitter.mcmc(guess_mo, data[1], mcmc_kws=mcmc_kws, 
-                                         npixels=100)
-    print(f"mieonly mcmc took {tick_tock()}")
+    mcmc_mo, fit_mo = mieonlyFitter.mcmc(
+        guess_mo, data[1], mcmc_kws=mcmc_kws, npixels=100)
+    print("mieonly mcmc took {}".format(tick_tock()))
     inout.save_pickle(fit_mo, 'ps_fit_mo_pt.pkl')
     inout.save_pickle(mcmc_mo, 'ps_mcmc_mo_pt.pkl')
 
     print("Starting mielens mcmc")
     tick_tock()
-    mcmc_ml, fit_ml = mielensFitter.mcmc(guess_ml, data[1], mcmc_kws=mcmc_kws,
-                                         npixels=100)
-    print(f"mielens mcmc took {tick_tock()}")
+    mcmc_ml, fit_ml = mielensFitter.mcmc(
+        guess_ml, data[1], mcmc_kws=mcmc_kws, npixels=10000)
+    print("mielens mcmc took {}".format(tick_tock()))
+    # With 10,000 px (all of them), it takes 24 s / step to run MCMC,
+    # with 5 temps, 100 walkers, 4 workers
     inout.save_pickle(fit_ml, 'ps_fit_ml_pt.pkl')
     inout.save_pickle(mcmc_ml, 'ps_mcmc_ml_pt.pkl')
 
-    
-    
