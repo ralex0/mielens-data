32e64607e16e37e65702b67461e713192dd7baf1
Brian Leahy <bdl48@cornell.edu>
2019-04-25 21:05:29 -0400

    mcmc_all_si_data: Running many images, not all

--- mcmc_all_silica_data.py
+++ mcmc_all_silica_data.py
@@ -1,83 +1,65 @@
-"""
-Idea: Mielens is ~N, where N is the # of px in the subset. So we just
-drop the # of pixels down to 1000 and do parallel tempering with a decent
-number of temperatures, to see if we can get better fits.
-"""
 from datetime import timedelta
 import time
 
 from lmfitFitter import Fitter
 import inout
 
+
 WHICH_SPHERE = 'silica'
 SIZE = 250
 THEORY = 'mieonly'
-NPIXELS = int(1e3)
-MCMC_KWS = {'burn': 0, 'steps': 3000, 'nwalkers': 100,
-            'thin': 1, 'workers': 16, 'ntemps': 5}
-FITTER = Fitter(theory=THEORY, quiet=True)
+MCMC_KWS = {'burn': 0, 'steps': 1000, 'nwalkers': 100,
+            'thin': 1, 'workers': 16, 'ntemps': 7}
 
 
-def time_mcmc(data, initial_guesses):
-    start_fit = time.time()
-    best_fit = FITTER.fit(data, initial_guesses)
-    end_fit = time.time()
-
-    copied_kwargs = MCMC_KWS.copy()
-    mcmc_times = {}
-    for steps in [1, 10]:
-        start_mcmc = time.time()
-        copied_kwargs.update({'steps': steps})
-        _ = FITTER._mcmc(
-            best_fit, data, mcmc_kws=copied_kwargs, npixels=NPIXELS)
-        end_mcmc = time.time()
-        mcmc_times.update({steps: end_mcmc - start_mcmc})
-    print("Fitting time:\t{:.3} s".format(end_fit - start_fit))
-    for steps, times in mcmc_times.items():
-        print("{} MCMC steps:\t{:.3} s".format(steps, times))
-
-
-def save_stuff(optimization_result, savefolder, which_image):
-    mcmc_ml = optimization_result['mcmc_result']
-    fit_ml = optimization_result['lmfit_result']
-    best_ml = optimization_result['best_result']
-
-    prefix = 'image-{}'.format(which_image)
-    tosave = {'fit': fit_ml, 'mcmc': mcmc_ml, 'best': best_ml}
-    for suffix, obj in tosave.items():
-        filename = os.path.join(savefolder, prefix + '-{}.pkl'.format(suffix))
-        inout.save_pickle(obj, filename)
-
-
-if __name__ == '__main__':
+def analyze_one_image(which_image):
+    which_fit = str(which_image)
     if WHICH_SPHERE == 'polystyrene':
         data_list, zpos = inout.load_polystyrene_sedimentation_data(
-            size=SIZE, holonums=None)
+            size=SIZE, holonums=[which_image])
         fits_mo, fits_ml = inout.load_polystyrene_sedimentation_params("03-27")
     elif WHICH_SPHERE == 'silica':
         data_list, zpos = inout.load_silica_sedimentation_data(
-            size=SIZE, holonums=None)
+            size=SIZE, holonums=[which_image])
         fits_mo, fits_ml = inout.load_silica_sedimentation_params("04-02")
-    guesses = fits_mo if THEORY == 'mieonly' else fits_ml
+    data = data_list[0]
 
-    savefolder = '{}-{}-size={}-npx={}'.format(
-        WHICH_SPHERE, THEORY, SIZE, NPIXELS)
-    if not os.path.isdir(savefolder):
-        os.mkdir(savefolder)
+    if THEORY == 'mieonly':
+        guess = fits_mo[which_fit]
+    elif THEORY == 'mielens':
+        guess = fits_ml[which_fit]
 
-    time_mcmc(data_list[0], guesses['0'])
-    raise ValueError
-    for which_image, (data, guess) in enumerate(zip(data_list, guesses.values)):
+    fitter = Fitter(theory=THEORY)
+    # WTF? 10000 pixels takes 10 s / iteration,
+    # 14400 px takes 48.74 s / iteration. So, 1e4 px:
+    npixels = int(1e4)
 
-        start_time = time.time()
-        print("Starting {} mcmc, image {}".format(THEORY, which_image))
-        optimization_result = FITTER.mcmc(
-            guess, data, mcmc_kws=MCMC_KWS, npixels=NPIXELS)
+    print("Starting {} mcmc".format(THEORY))
+    tick_tock()
+    optimization_result = fitter.mcmc(
+        guess, data, mcmc_kws=MCMC_KWS, npixels=npixels)
 
-        end_time = time.time()
-        duration = timedelta(seconds=end_time - start_time)
-        print("{} mcmc took {}".format(THEORY, duration))
+    print("{} mcmc took {}".format(THEORY, tick_tock()))
+    # With 10,000 px (all of them), it takes 24 s / step to run MCMC,
+    # with 5 temps, 100 walkers, 4 workers
+    mcmc_ml = optimization_result['mcmc_result']
+    fit_ml = optimization_result['lmfit_result']
 
-        # Saving things...
-        save_stuff(optimization_result, savefolder, which_image)
+    best_ml = optimization_result['best_result']
+
+    prefix = '{}-{}-frame={}-size={}-npx={}'.format(
+        WHICH_SPHERE, THEORY, which_image, SIZE, npixels)
+
+    tosave = {'fit': fit_ml, 'mcmc': mcmc_ml, 'best': best_ml}
+    for suffix, obj in tosave.items():
+        filename = prefix + '-{}.pkl'.format(suffix)
+        inout.save_pickle(obj, filename)
+
+
+
+# TODO:
+# Time mielens with npixels=1e2 vs npixels=1e3 vs npixels=1e4
+if __name__ == '__main__':
+    for which_image in [8, 9, 11, 12, 13]:
+        analyze_one_image(which_image)
 
