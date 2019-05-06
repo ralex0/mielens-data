8c2fb4dda3d360b0e97819d0b482d270f037783c
Brian Leahy <bdl48@cornell.edu>
2019-05-06 17:46:59 -0400

    Start total rewrite of make_mcmc_figure

--- make_mcmc_figure.py
+++ make_mcmc_figure.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+from scipy.stats import gaussian_kde
 
 import matplotlib as mpl; mpl.rcdefaults()
 import matplotlib.gridspec as gridspec
@@ -13,188 +14,111 @@ from pandas import DataFrame
 import inout
 
 
-class MCMCJointPlotFigure(object):
-    def __init__(self, result, burnin=0, joint_kdes=None):
-        """
-        Parameters
-        ----------
-        result : lmfit.minimizer.MinimizerResult
-        burning : int
-            Can also be negative. e.g. burnin=100 throws out the first 100
-            steps, burnin=-100 takes only the last 100 steps.
-        """
-        self.result = result
-        self.burnin = burnin
 
-        self.flatchain = self._create_flat_chain()
+class KDEEstimator(object):
+    def __init__(self, samples, names):
+        self.samples = samples
+        self.names = list(names)
 
-    def _create_flat_chain(self):
-        flattened = self._burn_in()
-        return DataFrame(flattened, columns=self.result.var_names)
+    def estimate_univariate_density(self, name):
+        index = self.names.index(name)
+        samples = self.samples[:, index]
+        kernel_callable = gaussian_kde(samples)
+        return kernel_callable
 
-    def _burn_in(self):
-        if self.result.chain.ndim == 4:  # parallel tempering
-            samples = self.result.chain[0]
-        elif self.result.chain.ndim == 3:  # normal mcmc
-            samples = self.result.chain
+    def estimate_bivariate_density(self, name1, name2):
+        index1 = self.names.index(name1)
+        index2 = self.names.index(name2)
+        samples = self.samples[:, [index1, index2]]
+        kernel_callable = gaussian_kde(samples)
+        return kernel_callable
+
+
+
+class MCMCFigure(object):
+    figsize = (5.25, 1)
+
+    def __init__(self, samples, mode='mielens'):
+        self.samples = samples
+        self.mode = mode
+        if mode == 'mielens':
+            names = {'n', 'r', 'lens_angle'}
+        elif mode == 'mieonly':
+            names = {'n', 'r', 'alpha'}
         else:
-            raise ValueError("chain dimensions not understood")
-        flattened = samples[:, self.burnin:, :].transpose((1, 0, 2)).reshape(
-            (-1, self.result.nvarys))
-        return flattened
+            raise ValueError("mode must be one of mielens, mieonly")
+        self.names = names
 
-    def plot(self, var0, var1, var2, labels=None):
-        self.fig = plt.figure(figsize=(5.25, 1.75))
-        if labels is None:
-            labels = [var0, var1, var2]
-        self._setup_axes()
-        self._set_axes_style()
-        self._plot_joints(var0, var1, var2)  # this is the slow part
-        self._plot_marginals(var0, var1, var2)
-        self._setlabels(labels)
-        return self.fig
+        self.figure = plt.figure(figsize=self.figsize)
+        self.axes_dict = self._setup_axes()
+
+
+class AxesSetup(object):
+    def __init__(self, figsize=(8, 6), xpad=0.05, ypad=0.20):
+        self.figsize = figsize
+        self.figure = plt.figure(figsize=self.figsize)
+        self.xpad = xpad
+        self.ypad = ypad
+        self.axes = self._setup_axes()
 
     def _setup_axes(self):
-        gs = gridspec.GridSpec(1, 3)
-        gs01 = gridspec.GridSpecFromSubplotSpec(
-            6, 6, subplot_spec=gs[0], hspace=0, wspace=0)
-        gs02 = gridspec.GridSpecFromSubplotSpec(
-            6, 6, subplot_spec=gs[1], hspace=0, wspace=0)
-        gs12 = gridspec.GridSpecFromSubplotSpec(
-            6, 6, subplot_spec=gs[2], hspace=0, wspace=0)
+        # Should make 2 sets of 6 axes, one for each pair each joint.
+        # Cohesion suggests this should be a separate object
+        #
+        axes_config = self._get_axes_config()
+        axes_left = self._transform_axes_to_box(
+            axes_config,
+            (0.5 * self.xpad, 0.5 * self.ypad, 0.5 - self.xpad, 1 - self.ypad))
+        axes_right = self._transform_axes_to_box(
+            axes_config,
+            (.5 + .5 * self.xpad, .5 * self.ypad, .5 - self.xpad,
+             1 - self.ypad))
+        axes_dict = {}
+        axes_names = ['left', 'right']
+        for name, d in zip(axes_names, [axes_left, axes_right]):
+            for key, box in d.items():
+                ax = self.figure.add_axes(box)
+                axes_dict.update({'{}_{}'.format(name, key): ax})
+        return axes_dict
 
-        self._ax01_joint = plt.Subplot(
-            self.fig, gs01[1:, :-1])
-        self._ax01_margx = plt.Subplot(
-            self.fig, gs01[0, :-1], sharex=self._ax01_joint)
-        self._ax01_margy = plt.Subplot(
-            self.fig, gs01[1:, -1], sharey=self._ax01_joint)
+    def _get_axes_config(self):
+        # axes boxes are left, bottom, width, height
+        box_height = 1.0 / 2.3
+        box_width = 0.5
+        half_height = box_height / 2.0
+        half_width = 2 * half_height * self.figsize[1] / self.figsize[0]
+        axes_positions = {}
 
-        self._ax02_joint = plt.Subplot(self.fig, gs02[1:, :-1])
-        self._ax02_margx = plt.Subplot(self.fig, gs02[0, :-1],
-                                       sharex=self._ax02_joint)
-        self._ax02_margy = plt.Subplot(self.fig, gs02[1:, -1],
-                                       sharey=self._ax02_joint)
+        axes_positions.update(
+            {'lower_left_box': (0, 0, box_width, box_height),
+             'upper_left_box': (0, box_height, box_width, box_height),
+             'upper_right_box': (box_width, box_height, box_width, box_height)})
 
-        self._ax12_joint = plt.Subplot(self.fig, gs12[1:, :-1])
-        self._ax12_margx = plt.Subplot(self.fig, gs12[0, :-1],
-                                       sharex=self._ax12_joint)
-        self._ax12_margy = plt.Subplot(self.fig, gs12[1:, -1],
-                                       sharey=self._ax12_joint)
+        axes_positions.update(
+            {'lower_left_half': (box_width, 0, half_width, box_height),
+             'upper_left_half': (0, 2 * box_height, box_width, half_height),
+             'upper_right_half':
+                (box_width, 2 * box_height, box_width, half_height)})
 
-        all_axes = [self._ax01_joint, self._ax01_margx, self._ax01_margy,
-                    self._ax02_joint, self._ax02_margx, self._ax02_margy,
-                    self._ax12_joint, self._ax12_margx, self._ax12_margy]
+        return axes_positions
 
-        for ax in all_axes:
-            self.fig.add_subplot(ax)
+    def _transform_axes_to_box(self, axes, box):
+        box_left, box_bottom, box_width, box_height = box
+        new_axes = {}
+        for key, value in axes.items():
+            old_left, old_bottom, old_width, old_height = value
 
-    def _plot_joints(self, var0, var1, var2):
-        data = [np.array(self.flatchain[v].values) for v in [var0, var1, var2]]
-        data_paired = [
-            (data[0], data[1]),
-            (data[0], data[2]),
-            (data[1], data[2]),
-            ]
+            new_left = box_left + old_left * box_width
+            new_bottom = box_bottom + old_bottom * box_height
+            new_width = old_width * box_width
+            new_height = old_height * box_height
 
-        joint_axes = [self._ax01_joint, self._ax02_joint, self._ax12_joint]
-        for ax, data_pair in zip(joint_axes, data_paired):
-            plt.sca(ax)
-            sns.kdeplot(
-                data_pair[0], data_pair[1], shade=True, shade_lowest=False,
-                gridsize=50)
-            self._set_joint_axes_style(ax, *data_pair)
-
-    def _set_joint_axes_style(self, ax, x, y):
-        x_bw, y_bw = np.array((x.std(), y.std())) * 2.5
-        xmin = max(np.median(x) - x_bw, x.min())
-        xmax = min(np.median(x) + x_bw, x.max())
-        ymin = max(np.median(y) - y_bw, y.min())
-        ymax = min(np.median(y) + y_bw, y.max())
-
-        ax.set_xlim(xmin, xmax)
-        ax.set_ylim(ymin, ymax)
-
-        xticks = np.linspace(xmin, xmax, 5)
-        yticks = np.linspace(ymin, ymax, 5)
-
-        xticklabels = ["{:.2f}".format(tick) for tick in xticks]
-        yticklabels = ["{:.2f}".format(tick) for tick in yticks]
-
-        ax.set_xticks(xticks)
-        ax.set_yticks(yticks)
-        ax.set_xticklabels(xticklabels, {'family': 'serif', 'size': 6})
-        ax.set_yticklabels(yticklabels, {'family': 'serif', 'size': 6})
-
-        ax.margins(x=0, y=0)
-
-    def _plot_marginals(self, var0, var1, var2):
-        margx_axes = [self._ax01_margx, self._ax02_margx, self._ax12_margx]
-        margy_axes = [self._ax01_margy, self._ax02_margy, self._ax12_margy]
-
-        datax = [np.array(self.flatchain[v]) for v in [var0, var0, var1]]
-        datay = [np.array(self.flatchain[v]) for v in [var1, var2, var2]]
-
-        for ax, data in zip(margx_axes, datax):
-            plt.sca(ax)
-            sns.kdeplot(data, shade=True,)
-
-        for ax, data in zip(margy_axes, datay):
-            plt.sca(ax)
-            sns.kdeplot(data, shade=True, vertical=True)
-
-    def _setlabels(self, labels):
-        font = {'size': 8,
-                'family': 'serif'}
-        self._ax01_joint.set_xlabel(labels[0], font)
-        self._ax01_joint.set_ylabel(labels[1], font)
-        self._ax02_joint.set_xlabel(labels[0], font)
-        self._ax02_joint.set_ylabel(labels[2], font)
-        self._ax12_joint.set_xlabel(labels[1], font)
-        self._ax12_joint.set_ylabel(labels[2], font)
-
-    def _set_axes_style(self):
-        self._set_marginal_axes_style()
-        self.fig.tight_layout()
-
-    def _set_marginal_axes_style(self):
-        margx_axes = [self._ax01_margx, self._ax02_margx, self._ax12_margx]
-        margy_axes = [self._ax01_margy, self._ax02_margy, self._ax12_margy]
-
-        for ax in margx_axes:
-            plt.setp(ax.get_xticklabels(), visible=False)
-            plt.setp(ax.yaxis.get_majorticklines(), visible=False)
-            plt.setp(ax.yaxis.get_minorticklines(), visible=False)
-            plt.setp(ax.get_yticklabels(), visible=False)
-            ax.yaxis.grid(False)
-
-        for ax in margy_axes:
-            plt.setp(ax.get_yticklabels(), visible=False)
-            plt.setp(ax.xaxis.get_majorticklines(), visible=False)
-            plt.setp(ax.xaxis.get_minorticklines(), visible=False)
-            plt.setp(ax.get_xticklabels(), visible=False)
-            ax.xaxis.grid(False)
-
-        sns.utils.despine(self.fig)
-
-        for ax in margx_axes:
-            sns.utils.despine(ax=ax, left=True)
-        for ax in margy_axes:
-            sns.utils.despine(ax=ax, bottom=True)
+            new_axes.update(
+                {key: (new_left, new_bottom, new_width, new_height)})
+        return new_axes
 
 
-def plot_mcmc_samples(mcmc_result):
-    fig = plt.figure()
-    for i, var in enumerate(mcmc_result.var_names):
-        plt.subplot(len(mcmc_result.var_names), 1, i+1)
-        for chain in mcmc_result.chain.T[i].T:
-            plt.plot(chain)
-        plt.ylabel(var)
-    return fig
-
-
-
-if __name__ == '__main__':
+def fuckme():
     # data = inout.load_mcmc_result_PS_mielens()
     fitfolder = './fits/sedimentation/mcmc/'
     figures = []
@@ -224,4 +148,8 @@ if __name__ == '__main__':
         figures.append(fig)
     plt.show()
 
+
+if __name__ == '__main__':
+    axes_setup = AxesSetup(figsize=(5.25, 3))
+    plt.show()
 
