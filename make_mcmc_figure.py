429451dbd1753bedd3bde3ed45f7e87015c846e5
Brian Leahy <bdl48@cornell.edu>
2019-05-06 16:21:50 -0400

    some cleaning up of make_mcmc_fig

--- make_mcmc_figure.py
+++ make_mcmc_figure.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 
 import matplotlib as mpl; mpl.rcdefaults()
@@ -13,12 +15,33 @@ import inout
 
 class MCMCJointPlotFigure(object):
     def __init__(self, result, burnin=0, joint_kdes=None):
-        self.chain = self._burn_in(result.chain, burnin)
-        self.flatchain = DataFrame(self.chain.reshape(-1, result.nvarys),
-                                   columns=result.var_names)
+        """
+        Parameters
+        ----------
+        result : lmfit.minimizer.MinimizerResult
+        burning : int
+            Can also be negative. e.g. burnin=100 throws out the first 100
+            steps, burnin=-100 takes only the last 100 steps.
+        """
+        self.result = result
+        self.burnin = burnin
 
-    def _burn_in(self, chain, n):
-        return chain[:, n:, :]
+        self.flatchain = self._create_flat_chain()
+
+    def _create_flat_chain(self):
+        flattened = self._burn_in()
+        return DataFrame(flattened, columns=self.result.var_names)
+
+    def _burn_in(self):
+        if self.result.chain.ndim == 4:  # parallel tempering
+            samples = self.result.chain[0]
+        elif self.result.chain.ndim == 3:  # normal mcmc
+            samples = self.result.chain
+        else:
+            raise ValueError("chain dimensions not understood")
+        flattened = samples[:, self.burnin:, :].transpose((1, 0, 2)).reshape(
+            (-1, self.result.nvarys))
+        return flattened
 
     def plot(self, var0, var1, var2, labels=None):
         self.fig = plt.figure(figsize=(5.25, 1.75))
@@ -26,26 +49,26 @@ class MCMCJointPlotFigure(object):
             labels = [var0, var1, var2]
         self._setup_axes()
         self._set_axes_style()
-        self._plot_joints(var0, var1, var2)
+        self._plot_joints(var0, var1, var2)  # this is the slow part
         self._plot_marginals(var0, var1, var2)
         self._setlabels(labels)
         return self.fig
 
     def _setup_axes(self):
         gs = gridspec.GridSpec(1, 3)
+        gs01 = gridspec.GridSpecFromSubplotSpec(
+            6, 6, subplot_spec=gs[0], hspace=0, wspace=0)
+        gs02 = gridspec.GridSpecFromSubplotSpec(
+            6, 6, subplot_spec=gs[1], hspace=0, wspace=0)
+        gs12 = gridspec.GridSpecFromSubplotSpec(
+            6, 6, subplot_spec=gs[2], hspace=0, wspace=0)
 
-        gs01 = gridspec.GridSpecFromSubplotSpec(6, 6, subplot_spec=gs[0],
-                                                hspace=0, wspace=0)
-        gs02 = gridspec.GridSpecFromSubplotSpec(6, 6, subplot_spec=gs[1],
-                                                hspace=0, wspace=0)
-        gs12 = gridspec.GridSpecFromSubplotSpec(6, 6, subplot_spec=gs[2],
-                                                hspace=0, wspace=0)
-
-        self._ax01_joint = plt.Subplot(self.fig, gs01[1:, :-1])
-        self._ax01_margx = plt.Subplot(self.fig, gs01[0, :-1],
-                                       sharex=self._ax01_joint)
-        self._ax01_margy = plt.Subplot(self.fig, gs01[1:, -1],
-                                       sharey=self._ax01_joint)
+        self._ax01_joint = plt.Subplot(
+            self.fig, gs01[1:, :-1])
+        self._ax01_margx = plt.Subplot(
+            self.fig, gs01[0, :-1], sharex=self._ax01_joint)
+        self._ax01_margy = plt.Subplot(
+            self.fig, gs01[1:, -1], sharey=self._ax01_joint)
 
         self._ax02_joint = plt.Subplot(self.fig, gs02[1:, :-1])
         self._ax02_margx = plt.Subplot(self.fig, gs02[0, :-1],
@@ -67,14 +90,19 @@ class MCMCJointPlotFigure(object):
             self.fig.add_subplot(ax)
 
     def _plot_joints(self, var0, var1, var2):
-        data = [np.array(self.flatchain[v]) for v in [var0, var1, var2]]
-        data_paired = [(data[0], data[1]), (data[0], data[2]),
-                       (data[1], data[2])]
+        data = [np.array(self.flatchain[v].values) for v in [var0, var1, var2]]
+        data_paired = [
+            (data[0], data[1]),
+            (data[0], data[2]),
+            (data[1], data[2]),
+            ]
 
         joint_axes = [self._ax01_joint, self._ax02_joint, self._ax12_joint]
         for ax, data_pair in zip(joint_axes, data_paired):
             plt.sca(ax)
-            sns.kdeplot(data_pair[0], data_pair[1], shade=True)
+            sns.kdeplot(
+                data_pair[0], data_pair[1], shade=True, shade_lowest=False,
+                gridsize=50)
             self._set_joint_axes_style(ax, *data_pair)
 
     def _set_joint_axes_style(self, ax, x, y):
@@ -165,10 +193,35 @@ def plot_mcmc_samples(mcmc_result):
     return fig
 
 
+
 if __name__ == '__main__':
-    data = inout.load_mcmc_result_PS_mielens()
-    plotter = MCMCJointPlotFigure(data, burnin=200)
-    labels = ["refractive index", "radius (μm)", "angular aperture (rad)"]
-    fig = plotter.plot('n', 'r', 'lens_angle', labels = labels)
+    # data = inout.load_mcmc_result_PS_mielens()
+    fitfolder = './fits/sedimentation/mcmc/'
+    figures = []
+    modes = {
+        'mielens': {
+            'variables': [
+                'n', 'r', 'lens_angle'],
+            'labels': [
+                "refractive index", "radius (μm)", "angular aperture (rad)"],
+            },
+        'mieonly': {
+            'variables': [
+                'n', 'r', 'alpha'],
+            'labels': [
+                "refractive index", "radius (μm)", "field rescale"],
+            },
+        }
+
+    for mode in modes.keys():
+        filename = os.path.join(
+            fitfolder,
+            'polystyrene-{}-frame=2-size=250-npx=10000-mcmc.pkl'.format(mode))
+        data = inout.load_pickle(filename)
+        plotter = MCMCJointPlotFigure(data, burnin=-500)
+        fig = plotter.plot(
+            *modes[mode]['variables'], labels=modes[mode]['labels'])
+        figures.append(fig)
     plt.show()
+
 
